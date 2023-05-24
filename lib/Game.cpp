78e1bd35840ad16afc34b445c56dd9c2d2d3497e
neo-level <frederik@neo-level.com>
2023-05-24 11:45:20 +0200

    defined methods in source file, initialized properties in constructor

---
+++ lib/Game.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Frederik Desmet on 24/05/2023.
+//
+
+#include "Game.h"
+
+Game::Game()
+{
+	Window = nullptr;
+	IsRunning = false;
+}
+
+bool Game::Initialize()
+{
+	return false;
+}
+void Game::RunLoop()
+{
+
+}
+void Game::Shutdown()
+{
+
+}
+void Game::ProcessInput()
+{
+
+}
+void Game::UpdateGame()
+{
+
+}
+void Game::GenerateOutput()
+{
+
+}
