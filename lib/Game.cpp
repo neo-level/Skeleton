486f1dd2fdcf78434af2e403102d58c115a6f6cb
neo-level <frederik@neo-level.com>
2023-05-24 12:09:25 +0200

    create a window when initialising is successful and return true otherwise false to prevent the game from starting.

--- lib/Game.cpp
+++ lib/Game.cpp
@@ -14,7 +14,20 @@ Game::Game()
 
 bool Game::Initialize()
 {
-	return false;
+	if (int sdlResult{ SDL_Init(SDL_INIT_VIDEO) }; sdlResult != 0)
+	{
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return false;
+	}
+
+	Window = SDL_CreateWindow("Skeleton", 100, 100, 1024, 768, 0);
+	if (not Window)
+	{
+		SDL_Log("Failed to create window: %s", SDL_GetError());
+		return false;
+	}
+
+	return true;
 }
 void Game::RunLoop()
 {
